2df3298a7ea7a982185dd752a3c300e90c6d180e
zak <codedpills@gmail.com>
2021-08-02 19:50:52 +0000

    use site Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
 import Head from "next/head";
 
+import Layout from "@/components/layout/Layout";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css"
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Shortly</title>
-        <meta
-          name="description"
-          content="Build your brand's recognition and get detailed insights on how your links are performing."
-        />
-        <link rel="icon" href="/favicon-32x32.png" />
-      </Head>
+    <Layout>
+      <div className={styles.container}>
+        <Head>
+          <title>Shortly</title>
+          <meta
+            name="description"
+            content="Build your brand's recognition and get detailed insights on how your links are performing."
+          />
+          <link rel="icon" href="/favicon-32x32.png" />
+        </Head>
 
-      <h1>Url Shortener</h1>
-    </div>
+        <h1>Url Shortener</h1>
+      </div>
+    </Layout>
   );
 }
