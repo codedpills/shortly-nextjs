eca41bfb68378e7b5e0fdbe82e358b111d6ed5b9
zak <codedpills@gmail.com>
2021-08-04 08:38:35 +0000

    add statistics content

---
+++ components/statistics/statistics.data.ts
@@ -0,0 +1,32 @@
+import brandRecognition from "@/assets/images/icon-brand-recognition.svg";
+import detailedRecords from "@/assets/images/icon-detailed-records.svg";
+import fullyCustomizable from "@/assets/images/icon-fully-customizable.svg";
+
+export const statisticsData = [
+  {
+    title: `Brand Recognition`,
+    description: `
+                Boost your brand recognition with each click. 
+                Generic links don't mean a thing. Branded links help instill 
+                confidence in your content.
+    `,
+    image: brandRecognition,
+  },
+  {
+    title: "Detailed Records",
+    description: `
+                Get insights into who is clicking the clinks. 
+                Knowing when and where people engage with your content
+                helps inform better decisions. 
+    `,
+    image: detailedRecords,
+  },
+  {
+    title: "Fully Customizable",
+    description: `
+                Improve brand awareness and content discoverability through
+                customizable links, supercharging audience engagement.
+    `,
+    image: fullyCustomizable,
+  },
+];
