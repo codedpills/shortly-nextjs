46fc6fafa76f71c3c04d82b1d0c14a93b3a65f43
zak <codedpills@gmail.com>
2021-08-04 08:38:56 +0000

    add component to display statistics

---
+++ components/statistics/Statistics.tsx
@@ -0,0 +1,44 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import StatsCard from "./stats-card/StatsCard";
+import { statisticsData } from "./statistics.data";
+
+import statisticsStyles from "./Statistics.module.scss";
+
+const Statistics = () => {
+  const statsList = statisticsData.map((stat, idx) => {
+    return (
+      <Col
+        key={idx}
+        xs={12}
+        md={4}
+        style={{ paddingTop: `${2.5 * idx}rem` }}
+        className={statisticsStyles.col}
+      >
+        <StatsCard stats={stat} />
+      </Col>
+    );
+  });
+
+  return (
+    <div className={statisticsStyles.root}>
+      <Container>
+        <div className={statisticsStyles.heading}>
+          <h2>Advanced Statistics</h2>
+          <p>
+            Track how your links are performing across the web with our advanced
+            statistics dashboard.
+          </p>
+        </div>
+        <div className={statisticsStyles.linewrapper}>
+          <div className={statisticsStyles.line}></div>
+          <Row>{statsList}</Row>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Statistics;
