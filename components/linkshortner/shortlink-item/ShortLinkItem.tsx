d5cce9380269867286c159642493657b1fb3552f
zak <codedpills@gmail.com>
2021-08-04 11:19:23 +0000

    add ShortLinkItem

---
+++ components/linkshortner/shortlink-item/ShortLinkItem.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import shortLinkItemStyles from "./ShortLinkItem.module.scss";
+
+type ShortLinkItemProp = {
+  shortLinkInfo: { originalUrl: string; shortenedUrl: string };
+};
+
+const ShortLinkItem = ({
+  shortLinkInfo = { originalUrl: "very long link", shortenedUrl: "short link" },
+}: ShortLinkItemProp) => {
+  const { originalUrl, shortenedUrl } = shortLinkInfo;
+  const [copySuccess, setCopySuccess] = useState(false);
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(shortenedUrl);
+      setCopySuccess(true);
+      setTimeout(() => {
+        setCopySuccess(false);
+      }, 1500);
+    } catch (error) {
+      setCopySuccess(false);
+    }
+  };
+
+  return (
+    <Card className={shortLinkItemStyles.card}>
+      <Row>
+        <Col xs={12} md={6}>
+          <div className={shortLinkItemStyles.originalUrl}>{originalUrl}</div>
+        </Col>
+        <Col xs={12} md={4}>
+          <div className={shortLinkItemStyles.shortenedUrl}>{shortenedUrl}</div>
+        </Col>
+        <Col xs={12} md={2}>
+          <Button
+            className={`btn btn-custom-primary-small btn-block ${
+              copySuccess && shortLinkItemStyles.activebtn
+            }`}
+            onClick={copyToClipboard}
+          >
+            {!copySuccess ? "Copy" : "Copied"}
+          </Button>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default ShortLinkItem;
