93bd47d9e6d00769b42269e42a3963371b18c4bc
zak <codedpills@gmail.com>
2021-08-06 16:03:31 +0000

    add prop type

--- components/linkshortner/shortlinkform-wrapper/ShortLinkFormWrapper.tsx
+++ components/linkshortner/shortlinkform-wrapper/ShortLinkFormWrapper.tsx
@@ -2,7 +2,11 @@ import LinkForm from "@/components/forms/linkform/LinkForm";
 
 import formWrapperStyles from "./ShortLinkFormWrapper.module.scss";
 
-const ShortLinkFormWrapper = ({ createShortLink }) => {
+type ShortLinkFormPropTypes = {
+  createShortLink: (linkUrl: string, callback: () => void) => void;
+};
+
+const ShortLinkFormWrapper = ({ createShortLink }: ShortLinkFormPropTypes) => {
   return (
     <div className={formWrapperStyles.root}>
       <div className={formWrapperStyles.background}>
